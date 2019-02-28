8455b3b546f9dfa42630b44d7cad638bdf707abb
MarcelMax <marcel.max.peters@web.de>
2019-02-28 13:52:15 +0100

    Added Toolbar, Navigation up button

--- app/src/main/java/marcelmax/shoppingcart/view/CartFragment.java
+++ app/src/main/java/marcelmax/shoppingcart/view/CartFragment.java
@@ -1,6 +1,27 @@
 package marcelmax.shoppingcart.view;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import marcelmax.shoppingcart.R;
 
 public class CartFragment extends Fragment {
+
+    private Unbinder unbinder;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.cart_screen, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
+
+
+        return rootView;
+    }
+
 }
--- app/src/main/java/marcelmax/shoppingcart/view/MainActivity.java
+++ app/src/main/java/marcelmax/shoppingcart/view/MainActivity.java
@@ -1,24 +1,67 @@
 package marcelmax.shoppingcart.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import marcelmax.shoppingcart.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    public NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setupNavigation();
+
+    }
+
+    private void setupNavigation() {
+        setSupportActionBar(toolbar);
+        
+        //setup Backbutton
+        NavController navController = Navigation.findNavController(this, R.id.my_nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this,navController);
+        getSupportActionBar().setTitle(R.string.app_name);
     }
 
     @Override
     public boolean onSupportNavigateUp() {
+        return Navigation.findNavController(this,R.id.my_nav_host_fragment).navigateUp();
 
-        return super.onSupportNavigateUp();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.cartFragment) {
+            return NavigationUI.onNavDestinationSelected(item,
+                    Navigation.findNavController(this, R.id.my_nav_host_fragment));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     //todo https://www.journaldev.com/22299/android-jetpack-navigation-architecture
 }
